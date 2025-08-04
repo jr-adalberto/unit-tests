7416ae0e7f24a4c89a3b5fe6bc245b1186050aff
Adalberto da Silva Junior <sbetiin_@hotmail.com>
2025-08-04 19:44:59 -0300

    create data.sql

---
+++ src/test/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO mensagem (ID, USUARIO, CONTEUDO, DATA_CRIACAO, GOSTEI)
+VALUES ('88ace1ea-7cde-4276-a44b-ae3b0adead1d', 'James', 'Mensagem one', '2025-05-19 04:12:06.10000', '0'),
+       ('85e7f770-d4be-4622-9437-bc4c5c1c63b2', 'Eric', 'Mensagem two', '2025-06-30 00:05:00.10000', '0'),
+       ('a2674580-22e3-49e0-9dc1-0bc5a8f193e0', 'John', 'Mensagem three', '2025-07-26 10:00:11.10000', '0');
