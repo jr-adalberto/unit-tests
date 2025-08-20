2c60476821414af6b775371f0ead61a29c26a360
Adalberto da Silva Junior <sbetiin_@hotmail.com>
2025-08-20 15:55:21 -0300

    update class

--- src/main/java/br/api/tests/model/Mensagem.java
+++ src/main/java/br/api/tests/model/Mensagem.java
@@ -1,14 +1,18 @@
 package br.api.tests.model;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -21,19 +25,30 @@ import java.util.UUID;
 public class Mensagem {
 
     @Id
+    @GenericGenerator(name = "uuid")
     private UUID id;
 
-    @Column(nullable = false)
-    @NotEmpty(message = "usuário não pode esta vázio.")
+    @NotEmpty(message = "usuário não pode estar vazio")
     private String usuario;
 
-    @Column(nullable = false)
-    @NotEmpty(message = "conteúdo não pode esta vázio")
+    @NotEmpty(message = "conteúdo não pode estar vazio")
     private String conteudo;
 
-    @Builder.Default
-    private LocalDateTime dataCriacao = LocalDateTime.now();
+    @CreationTimestamp
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSSSS")
+    private LocalDateTime dataCriacao;
+
+    @CreationTimestamp
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSSSS")
+    private LocalDateTime dataAlteracao;
 
     @Builder.Default
     private int gostei = 0;
+
+    @PrePersist
+    public void prePersist() {
+        var timestamp = LocalDateTime.now();
+        dataCriacao = timestamp;
+        dataAlteracao = timestamp;
+    }
 }
