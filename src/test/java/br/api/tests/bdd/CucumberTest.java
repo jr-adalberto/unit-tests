99e4e70b6a2636a73e1345ef12210db2f2aedd8d
Adalberto da Silva Junior <sbetiin_@hotmail.com>
2025-10-02 21:42:21 -0300

    improvements

--- src/test/java/br/api/tests/bdd/CucumberTest.java
+++ src/test/java/br/api/tests/bdd/CucumberTest.java
@@ -1,11 +1,18 @@
 package br.api.tests.bdd;
-
+import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+import static io.cucumber.core.options.Constants.EXECUTION_DRY_RUN_PROPERTY_NAME;
+import static io.cucumber.core.options.Constants.FILTER_TAGS_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "br.api.tests.bdd")
+@ConfigurationParameter(key = EXECUTION_DRY_RUN_PROPERTY_NAME, value = "false")
+@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "not @ignore")
 public class CucumberTest {
 }
