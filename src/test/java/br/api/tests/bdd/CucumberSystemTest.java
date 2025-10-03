8d5c96ae106e364d3792ad59d0b10dd216f8dedc
Adalberto da Silva Junior <sbetiin_@hotmail.com>
2025-10-02 21:41:58 -0300

    rename class

--- src/test/java/br/api/tests/bdd/CucumberSystemTest.java
+++ src/test/java/br/api/tests/bdd/CucumberSystemTest.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @CucumberContextConfiguration
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
-public class SpringCucumberIntegrationTest {
+public class CucumberSystemTest {
 }
